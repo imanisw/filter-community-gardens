e4a3761e51f815427103584264f0e479019f7621
Imani Sebri <imani.white79@journalism.cuny.edu>
2022-03-16 20:32:34 -0400

    fourth commit

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-main.js 
+
     const select = document.querySelector(".filter");
 
 select.addEventListener("change", function (e) {
+    console.log("Selector function fired!")
     const chosen_borough = e.target.value;
     const currently_visible = document.querySelectorAll(".item.active");
-    const make_visible = document.querySelectorAll(`.item.${chosen_food}`);
+    const make_visible = document.querySelectorAll(`.item.${chosen_borough}`);
 
-    currently_visible.forEach(function (Borough) {
-        food.classList.remove("active");
+    currently_visible.forEach(function (borough) {
+        borough.classList.remove("active");
     });
 
-    make_visible.forEach(function (food) {
-        food.classList.add("active");
+    make_visible.forEach(function (borough) {
+        borough.classList.add("active");
     });
 });
